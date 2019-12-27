19ff973ed641da993ff47e77e3fd23c1a1f4d30c
Felipe Lelis <58122715+FelipeLelis@users.noreply.github.com>
2019-12-27 15:47:13 -0300

    Add files via upload

---
+++ TabuExecucao.R
@@ -0,0 +1,14 @@
+
+#install.packages("tabuSearch")
+
+library(tabuSearch)
+
+z = tabuSearch(size = 12, iters = 1000, objFunc = compras, 
+           listSize = 9, nRestarts = 10, repeatAll = 1, verbose = T)
+
+plot(z)
+
+summary(z)
+
+
+summary(z, verbose = TRUE)
